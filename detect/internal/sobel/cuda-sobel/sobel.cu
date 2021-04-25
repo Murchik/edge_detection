209b77df2da4213c3cbcef10861ee4d971c6dd84
Murchik <mur4ikoff@gmail.com>
2021-04-25 21:11:36 +0300

    start rewriting kernels using texture memory

--- detect/internal/sobel/cuda-sobel/sobel.cu
+++ detect/internal/sobel/cuda-sobel/sobel.cu
@@ -18,9 +18,8 @@
             return 1;                                                  \
         }                                                              \
     } while (0)
-
-__global__ void monochrome_kernel(const uchar4 *input, float *output,
-                                  int image_size, int w) {
+/*
+__global__ void monochrome_kernel(const uchar4 *input, float *output, int image_size, int w) {
     int i = threadIdx.x + blockDim.x * blockIdx.x;
     if (i < image_size) {
         float R = (float)input[i].x / 255.0;
@@ -37,8 +36,7 @@ __global__ void monochrome_kernel(const uchar4 *input, float *output,
     }
 }
 
-__global__ void gaussian_kernel(const float *input, float *output,
-                                int image_size, int w, int h) {
+__global__ void gaussian_kernel(const float *input, float *output, int image_size, int w, int h) {
     int idx = threadIdx.x + blockDim.x * blockIdx.x;
     int i = idx / w;
     int j = idx % w;
@@ -57,8 +55,7 @@ __global__ void gaussian_kernel(const float *input, float *output,
     }
 }
 
-__global__ void sobel_vertical_kernel(const float *input, float *output,
-                                      int image_size, int w, int h) {
+__global__ void sobel_vertical_kernel(const float *input, float *output, int image_size, int w, int h) {
     int idx = threadIdx.x + blockDim.x * blockIdx.x;
     int i = idx / w;
     int j = idx % w;
@@ -77,8 +74,7 @@ __global__ void sobel_vertical_kernel(const float *input, float *output,
     }
 }
 
-__global__ void sobel_horizontal_kernel(const float *input, float *output,
-                                        int image_size, int w, int h) {
+__global__ void sobel_horizontal_kernel(const float *input, float *output, int image_size, int w, int h) {
     int idx = threadIdx.x + blockDim.x * blockIdx.x;
     int i = idx / w;
     int j = idx % w;
@@ -97,21 +93,17 @@ __global__ void sobel_horizontal_kernel(const float *input, float *output,
     }
 }
 
-__global__ void root_kernel(const float *input_vertical,
-                            const float *input_horizontal, float *output,
-                            int image_size, int w) {
+__global__ void root_kernel(const float *input_vertical, const float *input_horizontal, float *output, int image_size, int w) {
     int idx = threadIdx.x + blockDim.x * blockIdx.x;
     int i = idx / w;
     int j = idx % w;
     if (idx < image_size) {
         *(output + j + w * i) =
-            sqrtf(powf(*(input_vertical + j + w * i), 2.0) +
-                  powf(*(input_horizontal + j + w * i), 2.0));
+            sqrtf(powf(*(input_vertical + j + w * i), 2.0) + powf(*(input_horizontal + j + w * i), 2.0));
     }
 }
 
-__global__ void conv_float_uchar4(const float *input, uchar4 *output,
-                                  int image_size, int w) {
+__global__ void conv_float_uchar4(const float *input, uchar4 *output, int image_size, int w) {
     int idx = threadIdx.x + blockDim.x * blockIdx.x;
     int i = idx / w;
     int j = idx % w;
@@ -121,76 +113,88 @@ __global__ void conv_float_uchar4(const float *input, uchar4 *output,
         *(output + j + w * i) = make_uchar4(Y, Y, Y, 0);
     }
 }
+*/
+
+__global__ void transformKernel(uchar4 *output, cudaTextureObject_t texObj, int width, int height) {
+    unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    float u = x / width;
+    float v = y / height;
+
+    uchar4 pixel = tex2D<uchar4>(texObj, u, v);
+
+    float R = pixel.x / 255.0f;
+    float G = pixel.y / 255.0f;
+    float B = pixel.z / 255.0f;
+
+    float Y_linear = 0.2126f * R + 0.7152f * G + 0.0722f * B;
+
+    float Y_sRGB;
+    if (Y_linear > 0.0031308f) {
+        Y_sRGB = 1.055f * powf(Y_linear, 1.0f / 2.4f) - 0.055f;
+    } else {
+        Y_sRGB = 12.92f * Y_linear;
+    }
+
+    Y_sRGB *= 255.0f;
+    output[y * width + x] = make_uchar4(Y_sRGB, Y_sRGB, Y_sRGB, 0);
+}
 
 int ApplySobel(uint32_t *data, int w, int h) {
-    cudaStream_t stream;
-    CHECK_CUDART_ERROR(cudaStreamCreate(&stream));
+    // Allocate CUDA array in device memory
+    cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<uchar4>();
+    cudaArray_t cuArray;
+    CHECK_CUDART_ERROR(cudaMallocArray(&cuArray, &channelDesc, w, h));
 
-    int image_size = w * h;
+    // Set pitch of the source (the width in memory in bytes of the 2D array pointed
+    // to by src, including padding), we dont have any padding
+    const size_t spitch = w * sizeof(uchar4);
+    // Copy data located at address h_data in host memory to device memory
+    CHECK_CUDART_ERROR(cudaMemcpy2DToArray(cuArray, 0, 0, data, spitch, w * sizeof(uchar4), h, cudaMemcpyHostToDevice));
 
-    int threadsPerBlock = 256;
-    int numBlocks = image_size / threadsPerBlock + 1;
+    // Specify texture
+    struct cudaResourceDesc resDesc;
+    memset(&resDesc, 0, sizeof(resDesc));
+    resDesc.resType = cudaResourceTypeArray;
+    resDesc.res.array.array = cuArray;
 
-    uchar4 *gpu_data_uchar4;
-    size_t image_byte_size = sizeof(uchar4) * image_size;
-    CHECK_CUDART_ERROR(cudaMalloc(&gpu_data_uchar4, image_byte_size));
-    float *gpu_data_float;
-    int image_float_size = sizeof(float) * image_size;
-    CHECK_CUDART_ERROR(cudaMalloc(&gpu_data_float, image_byte_size));
+    // Specify texture object parameters
+    struct cudaTextureDesc texDesc;
+    memset(&texDesc, 0, sizeof(texDesc));
+    texDesc.addressMode[0] = cudaAddressModeWrap;
+    texDesc.addressMode[1] = cudaAddressModeWrap;
+    texDesc.filterMode = cudaFilterModePoint;
+    texDesc.readMode = cudaReadModeElementType;
+    texDesc.normalizedCoords = 1;
 
-    float *output_gaussian;
-    CHECK_CUDART_ERROR(cudaMalloc(&output_gaussian, image_float_size));
-    float *output_vertical;
-    CHECK_CUDART_ERROR(cudaMalloc(&output_vertical, image_float_size));
-    float *output_horizontal;
-    CHECK_CUDART_ERROR(cudaMalloc(&output_horizontal, image_float_size));
+    // Create texture object
+    cudaTextureObject_t texObj = 0;
+    CHECK_CUDART_ERROR(cudaCreateTextureObject(&texObj, &resDesc, &texDesc, NULL));
 
-    // Copying data into GPU
-    CHECK_CUDART_ERROR(cudaMemcpyAsync(gpu_data_uchar4, data, image_byte_size,
-                                       cudaMemcpyHostToDevice, stream));
+    // Allocate result of transformation in device memory
+    uchar4 *output;
+    CHECK_CUDART_ERROR(cudaMalloc(&output, w * h * sizeof(uchar4)));
 
-    // Color (RGBA) to grayscale (float value) conversion
-    monochrome_kernel<<<numBlocks, threadsPerBlock, 0, stream>>>(
-        gpu_data_uchar4, gpu_data_float, image_size, w);
-    CHECK_CUDART_ERROR(cudaStreamSynchronize(stream));
+    // Invoke kernel
+    // int threadsperBlock = 256;
+    // int numBlocks = (w * h) / threadsperBlock + 1;
 
-    // Gaussian blur
-    gaussian_kernel<<<numBlocks, threadsPerBlock, 0, stream>>>(
-        gpu_data_float, output_gaussian, image_size, w, h);
-    CHECK_CUDART_ERROR(cudaStreamSynchronize(stream));
+    dim3 threadsperBlock(16, 16);
+    dim3 numBlocks((w + threadsperBlock.x - 1) / threadsperBlock.x,
+                   (h + threadsperBlock.y - 1) / threadsperBlock.y);
 
-    // Sobel vertical core
-    sobel_vertical_kernel<<<numBlocks, threadsPerBlock, 0, stream>>>(
-        output_gaussian, output_vertical, image_size, w, h);
-    CHECK_CUDART_ERROR(cudaStreamSynchronize(stream));
+    transformKernel<<<numBlocks, threadsperBlock>>>(output, texObj, w, h);
 
-    // Sobel horizontal core
-    sobel_horizontal_kernel<<<numBlocks, threadsPerBlock, 0, stream>>>(
-        output_gaussian, output_horizontal, image_size, w, h);
-    CHECK_CUDART_ERROR(cudaStreamSynchronize(stream));
+    // Copy data from device back to host
+    CHECK_CUDART_ERROR(cudaMemcpy(data, output, w * h * sizeof(uint32_t), cudaMemcpyDeviceToHost));
 
-    // Composing vertical and horizontal outputs in an image
-    root_kernel<<<numBlocks, threadsPerBlock, 0, stream>>>(
-        output_vertical, output_horizontal, gpu_data_float, image_size, w);
-    CHECK_CUDART_ERROR(cudaStreamSynchronize(stream));
+    // Destroy texture object
+    CHECK_CUDART_ERROR(cudaDestroyTextureObject(texObj));
 
-    // Convert float data back to RGBA
-    conv_float_uchar4<<<numBlocks, threadsPerBlock, 0, stream>>>(
-        gpu_data_float, gpu_data_uchar4, image_size, w);
-    CHECK_CUDART_ERROR(cudaStreamSynchronize(stream));
-
-    // Copying output data into CPU
-    CHECK_CUDART_ERROR(cudaMemcpyAsync(data, gpu_data_uchar4, image_byte_size,
-                                       cudaMemcpyDeviceToHost, stream));
-
-    CHECK_CUDART_ERROR(cudaStreamSynchronize(stream));
-    CHECK_CUDART_ERROR(cudaStreamDestroy(stream));
-
-    CHECK_CUDART_ERROR(cudaFree(gpu_data_uchar4));
-    CHECK_CUDART_ERROR(cudaFree(gpu_data_float));
-    CHECK_CUDART_ERROR(cudaFree(output_gaussian));
-    CHECK_CUDART_ERROR(cudaFree(output_vertical));
-    CHECK_CUDART_ERROR(cudaFree(output_horizontal));
+    // Free device memory
+    CHECK_CUDART_ERROR(cudaFreeArray(cuArray));
+    CHECK_CUDART_ERROR(cudaFree(output));
 
     return 0;
 }
