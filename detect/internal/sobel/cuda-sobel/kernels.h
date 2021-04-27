75c422d4b97cf3c9a6b275ae2d38d6108c1b752e
Murchik <mur4ikoff@gmail.com>
2021-04-27 20:39:04 +0300

    Creating auxiliary functions

--- detect/internal/sobel/cuda-sobel/kernels.h
+++ detect/internal/sobel/cuda-sobel/kernels.h
@@ -5,20 +5,33 @@
 
 #include "cuda_runtime.h"
 
-__global__ void greyscaleKernel(cudaTextureObject_t texObj, cudaSurfaceObject_t outputSurfObj, int width, int height) {
-    // Calculate surface coordinates
+__global__ void toFloatKernel(cudaTextureObject_t inTexObj, cudaSurfaceObject_t outSurfObj, int width, int height) {
     unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
     unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-    float u = x / static_cast<float>(width);
-    float v = y / static_cast<float>(height);
+    if (x < width && y < height) {
+        float u = x / static_cast<float>(width);
+        float v = y / static_cast<float>(height);
+
+        uchar4 pixel_uchar4 = tex2D<uchar4>(inTexObj, u, v);
+        float4 pixel_float4 = make_float4(pixel_uchar4.x, pixel_uchar4.y, pixel_uchar4.z, pixel_uchar4.w);
+        surf2Dwrite(pixel_float4, outSurfObj, x * sizeof(float4), y);
+    }
+}
+
+__global__ void greyscaleKernel(cudaTextureObject_t inTexObj, cudaSurfaceObject_t outSurfObj, int width, int height) {
+    unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (x < width && y < height) {
-        uchar4 pixel = tex2D<uchar4>(texObj, u, v);;
+        float u = static_cast<float>(x) / static_cast<float>(width);
+        float v = static_cast<float>(y) / static_cast<float>(height);
+
+        float4 pixel = tex2D<float4>(inTexObj, u, v);
         
-        float R = static_cast<float>(pixel.x) / 255.0f;
-        float G = static_cast<float>(pixel.y) / 255.0f;
-        float B = static_cast<float>(pixel.z) / 255.0f;
+        float R =  pixel.x / 255.0f;
+        float G =  pixel.y / 255.0f;
+        float B =  pixel.z / 255.0f;
 
         float Y = 0.2126f * R + 0.7152f * G + 0.0722f * B;
 
@@ -29,49 +42,35 @@ __global__ void greyscaleKernel(cudaTextureObject_t texObj, cudaSurfaceObject_t 
         }
         Y *= 255.0f;
 
-        pixel = make_uchar4(Y, Y, Y, 0);
-        surf2Dwrite(pixel, outputSurfObj, x * 4, y);
+        surf2Dwrite(Y, outSurfObj, x * sizeof(float), y);
     }
 }
 
-/*
-__global__ void monochrome_kernel(const uchar4 *input, float *output, int image_size, int w) {
-    int i = threadIdx.x + blockDim.x * blockIdx.x;
-    if (i < image_size) {
-        float R = (float)input[i].x / 255.0;
-        float G = (float)input[i].y / 255.0;
-        float B = (float)input[i].z / 255.0;
+__global__ void gaussianKernel(cudaTextureObject_t inTexObj, cudaSurfaceObject_t outSurfObj, int width, int height) {
+    unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-        float Y_linear = 0.2126 * R + 0.7152 * G + 0.0722 * B;
-
-        if (Y_linear > 0.0031308) {
-            output[i] = 1.055 * powf(Y_linear, 1.0 / 2.4) - 0.055;
-        } else {
-            output[i] = 12.92 * Y_linear;
+    if (x < width && y < height) {
+        float gaussianFilterKernel[3][3] = { 1.0f, 2.0f, 1.0f, 
+                                             2.0f, 4.0f, 2.0f, 
+                                             1.0f, 2.0f, 1.0f };
+        int i, j;
+        float u, v;
+        float result = 0.0f;
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                u = (x + j - 1) / static_cast<float>(width);
+                v = (y + i - 1) / static_cast<float>(height);
+                result += tex2D<float>(inTexObj, u, v) * gaussianFilterKernel[i][j];
+            }
         }
+        result /= 16.0f;
+
+        surf2Dwrite(result, outSurfObj, x * sizeof(float), y);
     }
 }
 
-__global__ void gaussian_kernel(const float *input, float *output, int image_size, int w, int h) {
-    int idx = threadIdx.x + blockDim.x * blockIdx.x;
-    int i = idx / w;
-    int j = idx % w;
-    if (i < image_size && i > 1 && j > 1 && i < h - 1 && j < w - 1) {
-        *(output + j + w * i) = (*(input + (j - 1) + w * (i - 1)) * 1 +
-                                 *(input + (j    ) + w * (i - 1)) * 2 +
-                                 *(input + (j + 1) + w * (i - 1)) * 1 +
-
-                                 *(input + (j - 1) + w * (i    )) * 2 +
-                                 *(input + (j    ) + w * (i    )) * 4 +
-                                 *(input + (j + 1) + w * (i    )) * 2 +
-
-                                 *(input + (j - 1) + w * (i + 1)) * 1 +
-                                 *(input + (j    ) + w * (i + 1)) * 2 +
-                                 *(input + (j + 1) + w * (i + 1)) * 1) / 16.0;
-    }
-}
-
-__global__ void sobel_vertical_kernel(const float *input, float *output, int image_size, int w, int h) {
+/* __global__ void sobel_vertical_kernel(const float *input, float *output, int image_size, int w, int h) {
     int idx = threadIdx.x + blockDim.x * blockIdx.x;
     int i = idx / w;
     int j = idx % w;
@@ -117,18 +116,20 @@ __global__ void root_kernel(const float *input_vertical, const float *input_hori
         *(output + j + w * i) =
             sqrtf(powf(*(input_vertical + j + w * i), 2.0) + powf(*(input_horizontal + j + w * i), 2.0));
     }
-}
+}*/
 
-__global__ void conv_float_uchar4(const float *input, uchar4 *output, int image_size, int w) {
-    int idx = threadIdx.x + blockDim.x * blockIdx.x;
-    int i = idx / w;
-    int j = idx % w;
-    if (idx < image_size) {
-        float Y = *(input + j + w * i) * 255.0;
-        if (Y > 255.0) Y = 255.0;
-        *(output + j + w * i) = make_uchar4(Y, Y, Y, 0);
+__global__ void toRGBaKernel(cudaTextureObject_t inTexObj, cudaSurfaceObject_t outSurfObj, int width, int height) {
+    unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x < width && y < height) {
+        float u = x / static_cast<float>(width);
+        float v = y / static_cast<float>(height);
+
+        float pixel = tex2D<float>(inTexObj, u, v);
+        uchar4 pixel_uchar4 = make_uchar4(pixel, pixel, pixel, 0);
+        surf2Dwrite(pixel_uchar4, outSurfObj, x * sizeof(uchar4), y);
     }
 }
-*/
 
 #endif // KERNELS_H
--- detect/internal/sobel/cuda-sobel/sobel.cu
+++ detect/internal/sobel/cuda-sobel/sobel.cu
@@ -16,8 +16,7 @@
         }                                                              \
     } while (0)
 
-int createTextureObject(cudaTextureObject_t& TexObj, cudaArray_t& cuArray)
-{
+int createTextureObject(cudaTextureObject_t& TexObj, const cudaArray_t& cuArray) {
     struct cudaResourceDesc resDesc;
     memset(&resDesc, 0, sizeof(resDesc));
     resDesc.resType         = cudaResourceTypeArray;
@@ -25,9 +24,9 @@ int createTextureObject(cudaTextureObject_t& TexObj, cudaArray_t& cuArray)
 
     struct cudaTextureDesc texDesc;
     memset(&texDesc, 0, sizeof(texDesc));
-    texDesc.addressMode[0]   = cudaAddressModeWrap;
-    texDesc.addressMode[1]   = cudaAddressModeWrap;
-    texDesc.filterMode       = cudaFilterModePoint;
+    texDesc.addressMode[0]   = cudaAddressModeClamp;
+    texDesc.addressMode[1]   = cudaAddressModeClamp;
+    texDesc.filterMode       = cudaFilterModeLinear;
     texDesc.readMode         = cudaReadModeElementType;
     texDesc.normalizedCoords = 1;
 
@@ -36,57 +35,217 @@ int createTextureObject(cudaTextureObject_t& TexObj, cudaArray_t& cuArray)
     return 0;
 }
 
-int createSurfaceObject(cudaSurfaceObject_t& SurfObj, cudaArray_t& cuArray)
-{
+int createSurfaceObject(cudaSurfaceObject_t& SurfObj, cudaArray_t& cuArray) {
     struct cudaResourceDesc resDesc;
     memset(&resDesc, 0, sizeof(resDesc));
-    resDesc.resType = cudaResourceTypeArray;
-
+    resDesc.resType         = cudaResourceTypeArray;
     resDesc.res.array.array = cuArray;
+
     CHECK_CUDART_ERROR(cudaCreateSurfaceObject(&SurfObj, &resDesc));
 
     return 0;
 }
 
 template <typename T>
-int initializeCudaArray(cudaArray_t& cuArray, const void* data, int width, int height) {
+int createCudaArray(cudaArray_t& cuArray, int width, int height) {
     cudaChannelFormatDesc channel_desc = cudaCreateChannelDesc<T>();
     CHECK_CUDART_ERROR(cudaMallocArray(&cuArray, &channel_desc, width, height));
+    return 0;
+}
 
-    if (data) {
-        const size_t spitch = width * sizeof(T);
-        CHECK_CUDART_ERROR(cudaMemcpy2DToArray(cuArray, 0, 0, data, spitch, width * sizeof(T), height, cudaMemcpyHostToDevice));
-    }
+int applyToFloat(const cudaArray_t& inCuArray, cudaArray_t& outCuArray, int width, int height) {
+    dim3 threadsperBlock(16, 16);
+    dim3 numBlocks((width  + threadsperBlock.x - 1) / threadsperBlock.x,
+                   (height + threadsperBlock.y - 1) / threadsperBlock.y);
+
+    cudaTextureObject_t inTexObj = 0;
+    struct cudaResourceDesc resDesc;
+    memset(&resDesc, 0, sizeof(resDesc));
+    resDesc.resType         = cudaResourceTypeArray;
+    resDesc.res.array.array = inCuArray;
+
+    struct cudaTextureDesc texDesc;
+    memset(&texDesc, 0, sizeof(texDesc));
+    texDesc.addressMode[0]   = cudaAddressModeClamp;
+    texDesc.addressMode[1]   = cudaAddressModeClamp;
+    texDesc.filterMode       = cudaFilterModePoint;
+    texDesc.readMode         = cudaReadModeElementType;
+    texDesc.normalizedCoords = 1;
+
+    CHECK_CUDART_ERROR(cudaCreateTextureObject(&inTexObj, &resDesc, &texDesc, NULL));
+
+    cudaSurfaceObject_t outSurfObj = 0;
+    createSurfaceObject(outSurfObj, outCuArray);
+
+    toFloatKernel<<<numBlocks, threadsperBlock>>>(inTexObj, outSurfObj, width, height);
+    CHECK_CUDART_ERROR(cudaDeviceSynchronize());
+
+    CHECK_CUDART_ERROR(cudaDestroySurfaceObject(outSurfObj));
+    CHECK_CUDART_ERROR(cudaDestroyTextureObject(inTexObj));
 
     return 0;
 }
 
-int ApplySobel(uint32_t *data, int w, int h) {
+int applyGreyscale(const cudaArray_t& inCuArray, cudaArray_t& outCuArray, int width, int height) {
     dim3 threadsperBlock(16, 16);
-    dim3 numBlocks((w + threadsperBlock.x - 1) / threadsperBlock.x,
-                   (h + threadsperBlock.y - 1) / threadsperBlock.y);
+    dim3 numBlocks((width  + threadsperBlock.x - 1) / threadsperBlock.x,
+                   (height + threadsperBlock.y - 1) / threadsperBlock.y);
 
-    cudaArray_t gpuData;
-    initializeCudaArray<uchar4>(gpuData, data, w, h);
-
-    cudaTextureObject_t gpuDataTexObj = 0;
-    createTextureObject(gpuDataTexObj, gpuData);
-
-    cudaArray_t output;
-    initializeCudaArray<uchar4>(output, nullptr, w, h);
+    cudaTextureObject_t inTexObj = 0;
+    createTextureObject(inTexObj, inCuArray);
 
     cudaSurfaceObject_t outSurfObj = 0;
-    createSurfaceObject(outSurfObj, output);
+    createSurfaceObject(outSurfObj, outCuArray);
 
-    greyscaleKernel<<<numBlocks, threadsperBlock>>>(gpuDataTexObj, outSurfObj, w, h);
+    greyscaleKernel<<<numBlocks, threadsperBlock>>>(inTexObj, outSurfObj, width, height);
     CHECK_CUDART_ERROR(cudaDeviceSynchronize());
 
-    CHECK_CUDART_ERROR(cudaMemcpy2DFromArray(data, w * sizeof(uint32_t), output, 0, 0, w * sizeof(uchar4), h, cudaMemcpyDeviceToHost));
+    CHECK_CUDART_ERROR(cudaDestroySurfaceObject(outSurfObj));
+    CHECK_CUDART_ERROR(cudaDestroyTextureObject(inTexObj));
+
+    return 0;
+}
+
+int applyGaussian(const cudaArray_t& inCuArray, cudaArray_t& outCuArray, int width, int height) {
+    dim3 threadsperBlock(16, 16);
+    dim3 numBlocks((width + threadsperBlock.x - 1) / threadsperBlock.x,
+                   (height + threadsperBlock.y - 1) / threadsperBlock.y);
+
+    cudaTextureObject_t inTexObj = 0;
+    createTextureObject(inTexObj, inCuArray);
+
+    cudaSurfaceObject_t outSurfObj = 0;
+    createSurfaceObject(outSurfObj, outCuArray);
+
+    gaussianKernel<<<numBlocks, threadsperBlock>>>(inTexObj, outSurfObj, width, height);
+    CHECK_CUDART_ERROR(cudaDeviceSynchronize());
 
     CHECK_CUDART_ERROR(cudaDestroySurfaceObject(outSurfObj));
+    CHECK_CUDART_ERROR(cudaDestroyTextureObject(inTexObj));
 
-    CHECK_CUDART_ERROR(cudaFreeArray(output));
+    return 0;
+}
+
+// int applySobel(const cudaArray_t& inCuArray, cudaArray_t& outCuArray, int width, int height) {
+//     dim3 threadsperBlock(16, 16);
+//     dim3 numBlocks((width + threadsperBlock.x - 1) / threadsperBlock.x,
+//                    (height + threadsperBlock.y - 1) / threadsperBlock.y);
+
+//     // Applying vertical kernel of the Sobel filter
+//     cudaTextureObject_t inTexObj = 0;
+//     createTextureObject(inTexObj, inCuArray);    
+
+//     cudaArray_t outSobelHorizontal;
+//     createCudaArray<float>(outSobelHorizontal, nullptr, width, height);
+//     cudaSurfaceObject_t outSobelHorizontalSurfObj;
+//     createSurfaceObject(outSobelHorizontalSurfObj, outSobelHorizontal);
+
+//     sobelHorizontalKernel<<<numBlocks, threadsperBlock>>>(inTexObj, outSobelHorizontalSurfObj, width, height);
+//     CHECK_CUDART_ERROR(cudaDeviceSynchronize());
+
+//     CHECK_CUDART_ERROR(cudaDestroySurfaceObject(outSobelHorizontalSurfObj));
+//     CHECK_CUDART_ERROR(cudaDestroyTextureObject(inTexObj));
+
+//     // Applying horizontal kernel of the Sobel filter
+//     cudaArray_t inCuArrayCopy;
+//     createCudaArray<float>(inCuArrayCopy, inCuArray, width, height);
+//     cudaTextureObject_t inTexObjCopy = 0;
+//     createTextureObject(inTexObjCopy, inCuArrayCopy);
+
+//     cudaArray_t outSobelVertical;
+//     createCudaArray<float>(outSobelVertical, nullptr, width, height);
+//     cudaSurfaceObject_t outSobelVerticalSurfObj;
+//     createSurfaceObject(outSobelVerticalSurfObj, outSobelVertical);
+
+//     sobelVerticalKernel<<<numBlocks, threadsperBlock>>>(inTexObjCopy, outSobelVerticalSurfObj, width, height);
+//     CHECK_CUDART_ERROR(cudaDeviceSynchronize());
+
+//     CHECK_CUDART_ERROR(cudaDestroySurfaceObject(outSobelVerticalSurfObj));
+//     CHECK_CUDART_ERROR(cudaDestroyTextureObject(inTexObjCopy));
+
+//     // Getting result of the Sobel filter
+//     cudaSurfaceObject_t outSurfObj;
+//     createSurfaceObject(outSurfObj, outCuArray);
+
+//     cudaTextureObject_t inHorizontalTexObj = 0;
+//     createTextureObject(inHorizontalTexObj, outSobelHorizontal);
+
+//     cudaTextureObject_t inVerticalTexObj = 0;
+//     createTextureObject(inVerticalTexObj, outSobelVertical);
+
+//     rootKernel<<<numBlocks, threadsperBlock>>>(inHorizontalTexObj, inVerticalTexObj, outSurfObj, width, height);
+//     CHECK_CUDART_ERROR(cudaDeviceSynchronize());
+
+//     CHECK_CUDART_ERROR(cudaDestroyTextureObject(inVerticalTexObj));
+//     CHECK_CUDART_ERROR(cudaDestroyTextureObject(inHorizontalTexObj));
+//     CHECK_CUDART_ERROR(cudaDestroySurfaceObject(outSurfObj));
+
+//     CHECK_CUDART_ERROR(cudaFreeArray(outSobelVertical));
+//     CHECK_CUDART_ERROR(cudaFreeArray(inCuArrayCopy));
+//     CHECK_CUDART_ERROR(cudaFreeArray(outSobelHorizontal));
+// }
+
+int convertToRGBA(const cudaArray_t& inCuArray, cudaArray_t& outCuArray, int width, int height) {
+    dim3 threadsperBlock(16, 16);
+    dim3 numBlocks((width + threadsperBlock.x - 1) / threadsperBlock.x,
+                   (height + threadsperBlock.y - 1) / threadsperBlock.y);
+
+    cudaTextureObject_t inTexObj = 0;
+    createTextureObject(inTexObj, inCuArray);
+
+    cudaSurfaceObject_t outSurfObj = 0;
+    createSurfaceObject(outSurfObj, outCuArray);
+
+    toRGBaKernel<<<numBlocks, threadsperBlock>>>(inTexObj, outSurfObj, width, height);
+    CHECK_CUDART_ERROR(cudaDeviceSynchronize());
+
+    CHECK_CUDART_ERROR(cudaDestroySurfaceObject(outSurfObj));
+    CHECK_CUDART_ERROR(cudaDestroyTextureObject(inTexObj));
+    
+    return 0;
+}
+
+int ApplySobel(uint32_t* data, int w, int h) {
+    // Copy data to device
+    cudaArray_t gpuData;
+    cudaChannelFormatDesc channel_desc = cudaCreateChannelDesc<uchar4>();
+    CHECK_CUDART_ERROR(cudaMallocArray(&gpuData, &channel_desc, w, h));
+
+    const size_t spitch = w * sizeof(uint32_t);
+    CHECK_CUDART_ERROR(cudaMemcpy2DToArray(gpuData, 0, 0, data, spitch, w * sizeof(uchar4), h, cudaMemcpyHostToDevice));
+        
+    cudaArray_t gpuDataFloat4;
+    createCudaArray<float4>(gpuDataFloat4, w, h);
+    applyToFloat(gpuData, gpuDataFloat4, w, h);
     CHECK_CUDART_ERROR(cudaFreeArray(gpuData));
 
-    return 0; 
+    // Applying greyscale filter
+    cudaArray_t greyscaleOut;
+    createCudaArray<float>(greyscaleOut, w, h);
+    applyGreyscale(gpuDataFloat4, greyscaleOut, w, h);
+    CHECK_CUDART_ERROR(cudaFreeArray(gpuDataFloat4));
+
+    // Applying gaussian blur filter
+    cudaArray_t gaussianOut;
+    createCudaArray<float>(gaussianOut, w, h);
+    applyGaussian(greyscaleOut, gaussianOut, w, h);
+    CHECK_CUDART_ERROR(cudaFreeArray(greyscaleOut));
+
+    // // Applying sobel filter
+    // cudaArray_t sobelOut;
+    // createCudaArray<float>(sobelOut, w, h);
+    // applySobel(gaussianOut, sobelOut, w, h);
+    // CHECK_CUDART_ERROR(cudaFreeArray(gaussianOut));
+
+    // Convert back to RGBA
+    cudaArray_t output;
+    createCudaArray<uchar4>(output, w, h);
+    convertToRGBA(gaussianOut, output, w, h);
+    CHECK_CUDART_ERROR(cudaFreeArray(gaussianOut));
+
+    // Copy data back to host
+    CHECK_CUDART_ERROR(cudaMemcpy2DFromArray(data, w * sizeof(uint32_t), output, 0, 0, w * sizeof(uchar4), h, cudaMemcpyDeviceToHost));
+    CHECK_CUDART_ERROR(cudaFreeArray(output));
+
+    return 0;
 }
